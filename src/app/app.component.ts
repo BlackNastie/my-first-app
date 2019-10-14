dbedd269b74405678394b89fabe5011a2fd14403
Nastie Deminka <a.demina@bytexgames.ru>
2019-10-14 17:03:32 +0300

    lesson 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   headerText = 'Создание новой директивы';
+  items = [1,2,3,4,5];
+  current = 1;
+
+  onClick(number: number) {
+    this.current = number;
+  }
 }
