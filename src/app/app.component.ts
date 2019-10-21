3faaab0bdc828ac3fcbccb2fe01227df135be423
Black Nastie <blacknastie@mail.ru>
2019-10-21 22:28:42 +0300

    lesson 29

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core';
+import {of} from "rxjs";
+import {delay, subscribeOn} from "rxjs/operators";
+
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div class="col-xs-8 col-xs-offset-2">
+    <h1>{{title}}</h1>
+    <input class="form-control" type="text" [(ngModel)]="searchCar" >
+    <button class="btn btn-primary" (click)="addCar()">Add car!</button>
+    <hr>
+    <ul class="list-group">
+      <li class="list-group-item"
+      *ngFor="let car of cars | carFilter:searchCar: 'name'; let i = index;">
+        <b>{{ i + 1 }}</b> {{car.name}} <i>{{ car.desc }}</i>
+      </li>
+    </ul>
+  </div>`
 })
 
 export class AppComponent {
-  headerText = 'Создание новой директивы';
-  items = [1,2,3,4,5];
-  current = 1;
+  searchCar = '';
+  cars = [
+    {name: 'Ford', desc: 'desc 1'},
+    {name: 'Mazda', desc: 'desc 2'},
+    {name: 'Bentley', desc: 'desc 3'},
+    {name: 'BMW', desc: 'desc 4'},
+    {name: 'Audi', desc: 'desc 5'},
+    {name: 'Mercedec', desc: 'desc 6'},
+  ];
 
-  onClick(number: number) {
-    this.current = number;
+  title = '';
+
+  asyncTitle = of('Async title 3 sec')
+      .pipe(delay(3000))
+      .subscribe((str) => this.title = str);
+
+  addCar() {
+    this.cars.push({
+      name: "New Car",
+      desc: "WTF!"
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import {BackgroundDirective} from "./directives/background.directive";
+import {PowPipe} from "./pow.pipe";
+import { CarFilterPipe } from './car-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundDirective
+    PowPipe,
+    CarFilterPipe
   ],
   imports: [
     BrowserModule,
