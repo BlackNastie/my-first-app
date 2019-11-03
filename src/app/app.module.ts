63022146e3e223804c092d8bd3ff04a225837422
Black Nastie <blacknastie@mail.ru>
2019-11-03 22:04:23 +0300

    lesson 32

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import {PowPipe} from "./pow.pipe";
-import { CarFilterPipe } from './car-filter.pipe';
+import { HttpClientModule} from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { CarAddComponent } from './car-add/car-add.component';
+import {ConsoleService} from "./console.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PowPipe,
-    CarFilterPipe
+    CarComponent,
+    CarAddComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ConsoleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
